c2dede165f930c53948689140fbad8e3f168751d
devtushar001 <tc341269@gmail.com>
2024-10-04 00:16:13 -0700

    Array Methods

---
+++ backup/app.js
@@ -0,0 +1,34 @@
+console.log('Hello Tusahr');
+// Array Methods
+// 1. Foreach
+// 2. Filter
+// 3. Map
+// 4. Reduce
+// 5. fill
+// 6. Push
+
+
+const Number = [7, 5, 8, 9, 11, 17, 21, 13];
+// const NewNumber = Number.push(23);  // we can't push a number to array like this
+// console.log(NewNumber)  // it's return the length of array
+// Number.push(17);
+// console.log(Number);
+
+// Number.pop()
+// console.log(Number); // [7, 5, 8, 9, 11, 17, 21]
+
+// const newNumber = Number.pop();
+// console.log(newNumber) // 13
+
+// Number.shift();
+// console.log(Number) // [5, 8, 9, 11, 17, 21, 13]
+
+// const newNumber = Number.shift();
+// console.log(newNumber);  // 7
+
+// Number.unshift(12);
+// console.log(Number); // [12, 7, 5, 8, 9, 11, 17, 21, 13]
+
+// const newNumber = Number.unshift(12);
+// console.log(newNumber); // 9
+
